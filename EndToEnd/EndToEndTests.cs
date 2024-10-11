d9fec152cc3326f210962c00f5245f4220837a07
Troels-dat21B <troe1912@stud.kea.dk>
2024-10-11 14:43:20 +0200

    added another test

--- EndToEnd/EndToEndTests.cs
+++ EndToEnd/EndToEndTests.cs
@@ -4,14 +4,17 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace EndToEnd
 {
     public class EndToEndTests
     {
-        [Fact]
-        public void ReturnAFakeCPR()
+
+        private IWebDriver Driver()
         {
+
             // Initialize the Chrome WebDriver
             IWebDriver driver = new ChromeDriver();
 
@@ -21,9 +24,22 @@ namespace EndToEnd
             // Set window size
             driver.Manage().Window.Size = new System.Drawing.Size(949, 743);
 
+            return driver;
+        }
+
+
+        [Fact]
+        public void ReturnAFakeCPR()
+        {
+            IWebDriver driver = Driver();
+
+            //change to partial generation
+            IWebElement partialGenerationElement = driver.FindElement(By.CssSelector("#partialOptions > input"));
+            partialGenerationElement.Click();
+
             // Find the element by CSS selector and click
             IWebElement submitButton = driver.FindElement(By.CssSelector("#submit > input"));
-            //submitButton.Click();
+            submitButton.Click();
 
             // Wait for the element with the id "cpr" to be visible
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
@@ -34,6 +50,38 @@ namespace EndToEnd
 
             // Assert that the element with the id "cpr" is present
             Assert.NotNull(cprElement);
+        }
+
+        [Fact]
+        public void Return_a_fake_first_name_last_name_and_gender()
+        {
+
+            var driver = Driver();
+
+            IWebElement partialGenerationElement = driver.FindElement(By.CssSelector("#partialOptions > input"));
+            partialGenerationElement.Click();
+
+            //Selecting a value from the dropdown
+            SelectElement selectElement = new SelectElement(driver.FindElement(By.CssSelector("#cmbPartialOptions")));
+            selectElement.SelectByValue("name-gender");
+
+            IWebElement submitButton = driver.FindElement(By.CssSelector("#submit > input"));
+            submitButton.Click();
+
+
+            // Wait for the element with the id "cpr" to be visible
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+
+            //assigning the element to a variable
+            IWebElement firstNameElement = wait.Until(driver => driver.FindElement(By.CssSelector(".firstName")));
+            IWebElement lastNameElement = wait.Until(driver => driver.FindElement(By.CssSelector(".lastName")));
+            IWebElement dob = wait.Until(driver => driver.FindElement(By.CssSelector(".dob")));
+
+            Assert.NotNull(firstNameElement);
+            Assert.NotNull(lastNameElement);
+            Assert.NotNull(dob);
+
+
 
         }
     }
