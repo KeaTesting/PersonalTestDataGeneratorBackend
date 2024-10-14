131b1cc8502129f3c36e04b3290c5ed96c80bf94
chris <chri402s@stud.kea.dk>
2024-10-14 13:10:02 +0200

    added PostalCode entity

---
+++ PersonalTestDataGeneratorBackend/PostalCode.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalTestDataGeneratorBackend
+{
+    public class PostalCode
+    {
+        [Key]
+        public int PostCode { get; set; }
+        public string TownName { get; set; }
+
+        public PostalCode()
+        {
+
+        }
+    }
+}
