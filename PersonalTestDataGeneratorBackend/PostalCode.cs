a01db8c8bdbdc6d7ee1d100e04bf7e0e023333ed
Ceazer <90573869+Ceazer101@users.noreply.github.com>
2024-10-16 14:13:00 +0200

    Address generator unittest (#8)
    Added address generation, tests, and db

---
+++ PersonalTestDataGeneratorBackend/PostalCode.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalTestDataGeneratorBackend
+{
+    public class PostalCode
+    {
+        [Key]
+        public int PostCode { get; set; }
+        public string TownName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return PostCode + " " + TownName;
+            }
+        }
+
+        public PostalCode()
+        {
+
+        }
+    }
+}
